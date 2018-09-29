f7a6109299f0ed495d8e7494e987b9a5ad6c4d6d
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2018-09-29 11:58:56 -0700

    Update two_sum.hpp
    Remove commented out code, for past ideas for solution that didn't really pan out.

--- two_sum.hpp
+++ two_sum.hpp
@@ -10,35 +10,8 @@ public:
             return result;
         }
         
-        // figuring largest and smallest values not worth it
-        // auto firstLargest = nums[0];
-        // auto secondLargest = firstLargest;
-        // auto firstSmallest = firstLargest;
-        // auto secondSmallest = firstLargest;
-        // unordered_map<int, size_t> candidateValues;
         unordered_map<int, vector<size_t>> candidateValues;
         for (auto index = 0; index < nums.size(); ++index) {
-            // auto valueToCheck = nums[index];
-            // if (valueToCheck > firstLargest) {
-            //     secondLargest = firstLargest;
-            //     firstLargest = valueToCheck;
-            // } else if (valueToCheck > secondLargest) {
-            //     secondLargest = valueToCheck;
-            // }
-            // if (valueToCheck < firstSmallest) {
-            //     secondSmallest = firstSmallest;
-            //     firstSmallest = valueToCheck;
-            // } else if (valueToCheck < secondSmallest) {
-            //     secondSmallest = valueToCheck;
-            // }
-            // if (firstLargest + secondLargest == target
-            //    || firstSmallest + secondSmallest == target) {
-            //     result.push_back(0);
-            //     result.push_back(1);
-            //     return result;
-            // }
-            // filtering values not good idea, given 0 and negative values
-            //if (valueToCheck <= target) candidateValues[valueToCheck] = index;
             auto valueToStore = nums[index];
             if (candidateValues.find(valueToStore) == candidateValues.end()) {
                 vector<size_t> indexesOfValue;
@@ -53,15 +26,6 @@ public:
             if (candidateValue != otherValue) {
                 auto otherValueIterator = candidateValues.find(otherValue);
                 if (otherValueIterator != candidateValues.end()) {
-                    // auto candidateValueIndex = candidateValues[candidateValue][0];
-                    // auto otherValueIndex = otherValueIterator->second[0];
-                    // if (candidateValueIndex < otherValueIndex) {
-                    //     result.push_back(candidateValueIndex);
-                    //     result.push_back(otherValueIndex);
-                    // } else {
-                    //     result.push_back(otherValueIndex);
-                    //     result.push_back(candidateValueIndex);
-                    // }
                     result.push_back(candidateValues[candidateValue][0]);
                     result.push_back(otherValueIterator->second[0]);
                     return result;
@@ -71,32 +35,8 @@ public:
                 result.push_back(candidateValues[candidateValue][1]);
                 return result;
             }
-            // erasing values already seen not good idea, given repeated values
-            // candidateValues.erase(candidateValues.find(candidateValue));
         }
         // not yet sure how to make value null appear in output
         return result;
-        
-        // scanning in order in unordered map not order of input, so fails when multiple valid pairs
-        // while (candidateValues.size() > 0) {
-        //     auto candidateValueIterator = candidateValues.begin();
-        //     auto candidateValue = candidateValueIterator->first;
-        //     auto otherValue = target - candidateValue;
-        //     auto otherValueIterator = candidateValues.find(otherValue);
-        //     if (otherValueIterator != candidateValues.end()) {
-        //         auto candidateValueIndex = candidateValueIterator->second;
-        //         auto otherValueIndex = otherValueIterator->second;
-        //         if (candidateValueIndex < otherValueIndex) {
-        //             result.push_back(candidateValueIndex);
-        //             result.push_back(otherValueIndex);
-        //         } else {
-        //             result.push_back(otherValueIndex);
-        //             result.push_back(candidateValueIndex);
-        //         }
-        //         return result;
-        //     }
-        //     candidateValues.erase(candidateValueIterator);
-        // }
-        // return null;
     }
 };
