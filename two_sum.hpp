efe56e605de8eacfe1f15c7ae9214771d4fc037f
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2018-09-29 13:35:52 -0700

    Update two_sum.hpp
    To deal with negative values in the input array so as to get submission accepted and passing all tests, split values into 2 sparse arrays, one for the non-negative values, and one for the negative values.  A little bit messy code results without refactoring into separate methods.  Reason for 2 arrays is if all values were stored in a single array, would be too easy to end up allocating billions (if 32-bit) of array elements just to accommodate input with both positive and negative values.  Still doesn't pass all tests, because run time environment does not have enough memory for the test cases, which would require at least 50 million entries in the positive range.

--- two_sum.hpp
+++ two_sum.hpp
@@ -2,31 +2,35 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
-        vector<pair<bool, size_t>> candidateValues;
+        vector<pair<bool, size_t>> candidateNonNegativeValues;
+        vector<pair<bool, size_t>> candidateNegativeValues;
         for (auto index = 0; index < nums.size(); ++index) {
             auto candidateValue = nums[index];
-            if (candidateValues.size() > candidateValue && candidateValues[candidateValue].first) {
+            if (candidateValue >= 0 && candidateNonNegativeValues.size() > candidateValue && candidateNonNegativeValues[candidateValue].first
+                || candidateValue < 0 && candidateNegativeValues.size() > -candidateValue-1 && candidateNegativeValues[-candidateValue-1].first) {
                 if (candidateValue << 1 == target) {
-                    result.push_back(candidateValues[candidateValue].second);
+                    result.push_back((candidateValue >= 0 ? candidateNonNegativeValues[candidateValue] : candidateNegativeValues[-candidateValue-1]).second);
                     result.push_back(index);
                     return result;
                 }
             } else {
                 auto otherValue = target - candidateValue;
-                if (candidateValues.size() > otherValue && candidateValues[otherValue].first) {
-                    result.push_back(candidateValues[otherValue].second);
+                if (otherValue >= 0 && candidateNonNegativeValues.size() > otherValue && candidateNonNegativeValues[otherValue].first
+                    || otherValue < 0 && candidateNegativeValues.size() > -otherValue-1 && candidateNegativeValues[-otherValue-1].first) {
+                    result.push_back((otherValue >= 0 ? candidateNonNegativeValues[otherValue] : candidateNegativeValues[-otherValue-1]).second);
                     result.push_back(index);
                     return result;
                 }
             }
             pair<bool, size_t> pairToStore;
-            if (candidateValues.size() <= candidateValue) {
+            if (candidateValue >= 0 && candidateNonNegativeValues.size() <= candidateValue
+                || candidateValue < 0 && candidateNegativeValues.size() <= -candidateValue-1) {
                 pairToStore.first = false;
-                candidateValues.resize(candidateValue+1, pairToStore);
+                (candidateValue >= 0 ? candidateNonNegativeValues : candidateNegativeValues).resize(candidateValue >= 0 ? candidateValue+1 : -candidateValue, pairToStore);
             }
             pairToStore.first = true;
             pairToStore.second = index;
-            candidateValues[candidateValue] = pairToStore;
+            (candidateValue >= 0 ? candidateNonNegativeValues[candidateValue] : candidateNegativeValues[-candidateValue-1]) = pairToStore;
         }
         // not yet sure how to make value null appear in output
         return result;
