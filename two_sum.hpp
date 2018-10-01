37c12286e56943ade99c98d494d77b44359d045c
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2018-09-30 23:14:52 -0700

    Update two_sum.hpp
    Simplify logic to fold cases of 2 elements with same value into ones with different values, which could actually save some redundant searches as well as making code more readable.

--- two_sum.hpp
+++ two_sum.hpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
         unordered_map<int, size_t> candidateValues;
         for (auto index = 0; index < nums.size(); ++index) {
             auto candidateValue = nums[index];
-            auto candidateValueIterator = candidateValues.find(candidateValue);
-            if (candidateValueIterator != candidateValues.end()) {
-                if (candidateValue << 1 == target) {
-                    result.push_back(candidateValueIterator->second);
-                    result.push_back(index);
-                    return result;
-                }
-            } else {
-                auto otherValue = target - candidateValue;
-                auto otherValueIterator = candidateValues.find(otherValue);
-                if (otherValueIterator != candidateValues.end()) {
-                    result.push_back(otherValueIterator->second);
-                    result.push_back(index);
-                    return result;
-                }
+            auto otherValue = target - candidateValue;
+            auto otherValueIterator = candidateValues.find(otherValue);
+            if (otherValueIterator != candidateValues.end()) {
+                return {otherValueIterator->second, index};
             }
             candidateValues[candidateValue] = index;
         }
-        // not yet sure how to make value null appear in output
-        return result;
+        return {};
     }
 };
