36a1740c2b7a5b61989bb81e086790fb5a59041f
Gary Hildebrand <1429620+garyah@users.noreply.github.com>
2018-09-30 23:03:56 -0700

    Update two_sum.hpp
    Revert back to using unordered map instead of sparse array, given the limited memory available to run the solution.

--- two_sum.hpp
+++ two_sum.hpp
@@ -2,35 +2,26 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
-        vector<pair<bool, size_t>> candidateNonNegativeValues;
-        vector<pair<bool, size_t>> candidateNegativeValues;
+        unordered_map<int, size_t> candidateValues;
         for (auto index = 0; index < nums.size(); ++index) {
             auto candidateValue = nums[index];
-            if (candidateValue >= 0 && candidateNonNegativeValues.size() > candidateValue && candidateNonNegativeValues[candidateValue].first
-                || candidateValue < 0 && candidateNegativeValues.size() > -candidateValue-1 && candidateNegativeValues[-candidateValue-1].first) {
+            auto candidateValueIterator = candidateValues.find(candidateValue);
+            if (candidateValueIterator != candidateValues.end()) {
                 if (candidateValue << 1 == target) {
-                    result.push_back((candidateValue >= 0 ? candidateNonNegativeValues[candidateValue] : candidateNegativeValues[-candidateValue-1]).second);
+                    result.push_back(candidateValueIterator->second);
                     result.push_back(index);
                     return result;
                 }
             } else {
                 auto otherValue = target - candidateValue;
-                if (otherValue >= 0 && candidateNonNegativeValues.size() > otherValue && candidateNonNegativeValues[otherValue].first
-                    || otherValue < 0 && candidateNegativeValues.size() > -otherValue-1 && candidateNegativeValues[-otherValue-1].first) {
-                    result.push_back((otherValue >= 0 ? candidateNonNegativeValues[otherValue] : candidateNegativeValues[-otherValue-1]).second);
+                auto otherValueIterator = candidateValues.find(otherValue);
+                if (otherValueIterator != candidateValues.end()) {
+                    result.push_back(otherValueIterator->second);
                     result.push_back(index);
                     return result;
                 }
             }
-            pair<bool, size_t> pairToStore;
-            if (candidateValue >= 0 && candidateNonNegativeValues.size() <= candidateValue
-                || candidateValue < 0 && candidateNegativeValues.size() <= -candidateValue-1) {
-                pairToStore.first = false;
-                (candidateValue >= 0 ? candidateNonNegativeValues : candidateNegativeValues).resize(candidateValue >= 0 ? candidateValue+1 : -candidateValue, pairToStore);
-            }
-            pairToStore.first = true;
-            pairToStore.second = index;
-            (candidateValue >= 0 ? candidateNonNegativeValues[candidateValue] : candidateNegativeValues[-candidateValue-1]) = pairToStore;
+            candidateValues[candidateValue] = index;
         }
         // not yet sure how to make value null appear in output
         return result;
