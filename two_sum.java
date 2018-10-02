4f1013248353898a05261f4031d1e133c6fcb5a5
Gary Hildebrand <ghilde@microsoft.com>
2018-10-02 00:32:11 -0700

    Java implementation of solution.

---
+++ two_sum.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> candidateValues = new HashMap<>();
+        for (int index = 0; index < nums.length; index++) {
+            int candidateValue = nums[index];
+            int otherValue = target - candidateValue;
+            if (candidateValues.containsKey(otherValue)) {
+                return new int[] {map.get(otherValue), index};
+            }
+            candidateValues.put(candidateValue, index);
+        }
+        return null;
+    }
+};
